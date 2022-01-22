3d2b05a0c85d4dfad1821296ba9c0d059f01cd16
dp vpn <96043348+Wenisaputri@users.noreply.github.com>
2022-01-22 20:37:54 +0700

    Create ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://dpvpn.me/ip | grep $MYIP )
+IZIN=$( curl https://dpvpn.me/ip.json | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
