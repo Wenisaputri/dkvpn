a5ddf043b81f7a080351eef5d5785170cd93badf
dp vpn <96043348+Wenisaputri@users.noreply.github.com>
2022-01-23 13:05:30 +0700

    Update portwg.sh

--- ssh/portwg.sh
+++ ssh/portwg.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
+IZIN=$( curl https://dpvpn.me/ip.json| grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
